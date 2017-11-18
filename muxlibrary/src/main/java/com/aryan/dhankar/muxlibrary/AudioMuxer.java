e8baedd9d16518081cdf234fe61c28ac02a2e43f
dell <aryandhankar11@gmail.com>
2017-11-18 20:38:13 +0530

    Muxing Library

---
+++ muxlibrary/src/main/java/com/aryan/dhankar/muxlibrary/AudioMuxer.java
@@ -0,0 +1,9 @@
+package com.aryan.dhankar.muxlibrary;
+
+/**
+ * Created by user on 8/11/17.
+ */
+
+interface AudioMuxer {
+     boolean mux(String videoFile, String audioFile, String outputFile);
+}
---
+++ muxlibrary/src/main/java/com/aryan/dhankar/muxlibrary/Mp4ParserAudioMuxer.java
@@ -0,0 +1,70 @@
+package com.aryan.dhankar.muxlibrary;
+
+import com.coremedia.iso.boxes.Container;
+import com.googlecode.mp4parser.authoring.Movie;
+import com.googlecode.mp4parser.authoring.Track;
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by user on 8/11/17.
+ */
+
+public class Mp4ParserAudioMuxer implements AudioMuxer {
+    @Override
+    public boolean mux(String videoFile, String audioFile, String outputFile) {
+        Movie video;
+        try {
+            video = new MovieCreator().build(videoFile);
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        Movie audio;
+        try {
+            audio = new MovieCreator().build(audioFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        Track audioTrack = audio.getTracks().get(0);
+        video.addTrack(audioTrack);
+
+        Container out = new DefaultMp4Builder().build(video);
+
+        FileOutputStream fos;
+
+        try {
+            fos = new FileOutputStream(outputFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        BufferedWritableFileByteChannel byteBufferByteChannel =
+                new BufferedWritableFileByteChannel(fos);
+
+        try {
+            out.writeContainer(byteBufferByteChannel);
+            byteBufferByteChannel.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+}
